65d968e39043ec0b55d08e78edd01a5cff899ac0
Dimitri Meavilla <meavilla.73@gmail.com>
2023-03-21 14:08:45 +0100

    ok

--- javascript/data.js
+++ javascript/data.js
@@ -1,12 +1,12 @@
 const data = [
     {
         id: 1,
-        title: 'project',
+        title: 'Comparison: Real versus AI',
         student: "Dimitri Meavilla",
-        anchor: "#lien",
-        real_picture: "",
-        generate_picture: "",
-        decription: "A photo of a hill filled with trees, with a Chinese-style palace building overlooking the hill.In the foreground, at the bottom of the photo, a stone fence borders the foot of the hill along with a Chinese temple-style cabin. The photo is taken at dusk, with a cloudy sky, and the orange rays of the sun reflecting on some of the clouds",
+        anchor: "#projet1",
+        real_picture: "real_picture_Dimitri.jpg",
+        generate_picture: "generate_picture_Dimitri.png",
+        description: "A photo of a hill filled with trees, with a Chinese-style palace building overlooking the hill.In the foreground, at the bottom of the photo, a stone fence borders the foot of the hill along with a Chinese temple-style cabin. The photo is taken at dusk, with a cloudy sky, and the orange rays of the sun reflecting on some of the clouds",
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ const data = [
         anchor: "#lien",
         real_picture: "",
         generate_picture: "",
-        decription: "A photo of a hill filled with trees, with a Chinese-style palace building overlooking the hill.In the foreground, at the bottom of the photo, a stone fence borders the foot of the hill along with a Chinese temple-style cabin. The photo is taken at dusk, with a cloudy sky, and the orange rays of the sun reflecting on some of the clouds",
+        description: "A photo of a hill filled with trees, with a Chinese-style palace building overlooking the hill.In the foreground, at the bottom of the photo, a stone fence borders the foot of the hill along with a Chinese temple-style cabin. The photo is taken at dusk, with a cloudy sky, and the orange rays of the sun reflecting on some of the clouds",
 
     }
 ]
--- javascript/pages.js
+++ javascript/pages.js
@@ -1,11 +1,44 @@
-function article(obj) {
-    let title = document.createElement('h1')
-    let description = document.createElement('p')
-    let real_picture = document.createElement('img')
-    let generate_picture = document.createElement('img')
+function createSlide({ real_picture, title, description, anchor, generate_picture }) {
+    const slide = document.createElement('section');
+    slide.classList.add('slide');
+    slide.id = anchor;
+    const titleElement = document.createElement('h2');
+    titleElement.innerText = title;
+    slide.appendChild(titleElement);
+    for (let i = 0; i < 3; i++) {
+        const article = document.createElement('article')
+        switch (i) {
+            case 0:
 
+                const image = document.createElement('img');
+                image.src = "./asset/" + real_picture;
+                image.alt = description;
+                article.appendChild(titleElement);
+
+                article.appendChild(image);
+                break;
+            case 1:
+                const descriptionElement = document.createElement('p');
+                descriptionElement.innerText = description;
+                article.appendChild(titleElement);
+
+                article.appendChild(descriptionElement);
+                break;
+            case 2:
+                const image2 = document.createElement('img');
+                image2.src = "./asset/" + generate_picture;
+                image2.alt = description;
+                article.appendChild(titleElement);
+
+                article.appendChild(image2);
+                break;
+            default:
+                break;
+        }
+        slide.appendChild(article)
+    }
+    return slide;
 }
-
 function nav_func(obj) {
     const nav = document.querySelector('#nav')
     console.log(nav);
@@ -13,11 +46,35 @@ function nav_func(obj) {
         const anchor = document.createElement('a')
         if (Object.hasOwnProperty.call(obj, key)) {
             const element = obj[key];
-            console.log(element);
-            anchor.text = element.title + " " + element.id
+            anchor.text = element.title
             anchor.href = element.anchor
         }
         nav.appendChild(anchor)
     }
+}
+
+function goToSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const navigationButtons = document.querySelectorAll('.navigation-button');
+
+    // Masquer toutes les slides et les boutons de navigation
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+
+    navigationButtons.forEach((button) => {
+        button.classList.remove('active');
+    });
+
+    // Afficher la slide spécifique
+    slides[index].style.display = 'block';
+    navigationButtons[index].classList.add('active');
+}
+
+
+
+
+for (let i = 0; i < data.length; i++) {
+    document.querySelector('main').appendChild(createSlide(data[i]))
 }
 nav_func(data)
